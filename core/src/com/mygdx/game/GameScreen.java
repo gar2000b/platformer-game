7eb0018ca03b87780b9e21a86bd14e57bb3524cd
Gary Black <gar2000b@yahoo.com>
2016-10-27 22:25:03 -0400

    Now rendering blank screen

---
+++ core/src/com/mygdx/game/GameScreen.java
@@ -0,0 +1,80 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+/**
+ * Created by Digilogue on 27/10/2016.
+ */
+public class GameScreen extends ScreenAdapter {
+
+    private static final float WORLD_WIDTH = 640;
+    private static final float WORLD_HEIGHT = 480;
+
+    private ShapeRenderer shapeRenderer;
+    private Viewport viewport;
+    private Camera camera;
+    private SpriteBatch batch;
+    private final PeteGame peteGame;
+
+    public GameScreen(PeteGame peteGame) {
+        this.peteGame = peteGame;
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        viewport.update(width, height);
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        camera = new OrthographicCamera();
+        camera.position.set(WORLD_WIDTH / 2, WORLD_HEIGHT / 2, 0);
+        camera.update();
+        viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+        shapeRenderer = new ShapeRenderer();
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update(delta);
+        clearScreen();
+        draw();
+        drawDebug();
+    }
+
+    private void drawDebug() {
+        shapeRenderer.setProjectionMatrix(camera.projection);
+        shapeRenderer.setTransformMatrix(camera.view);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.end();
+    }
+
+    private void draw() {
+        batch.setProjectionMatrix(camera.projection);
+        batch.setTransformMatrix(camera.view);
+        batch.begin();
+        batch.end();
+    }
+
+    private void clearScreen() {
+        Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    private void update(float delta) {
+
+    }
+}
